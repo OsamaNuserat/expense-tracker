6b292f45465dda37617c781bde9f9f8eadc66965
OsamaNuserat <osamanuserat3@gmail.com>
2025-06-07 13:08:16 +0300

    cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors'); // ⬅️ import this
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors()); // ⬅️ enable CORS
 app.use(express.json());
 
 const messages = [];
